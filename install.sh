1a0663e7966f17ee5e68b577e14332a8b056b5b8
B3CKDOOR <B3CKDOOR@protonmail.com>
2021-01-23 17:19:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=d
 wget -O steam.deb http://media.steampowered.com/client/installer/steam.deb
 wget -O java8-default.deb https://debian.opennms.org/dists/opennms-25/main/binary-all/oracle-java8-set-default_8u131-1~webupd8~2_all.deb
 wget -O java8-installer.deb https://debian.opennms.org/dists/opennms-25/main/binary-all/oracle-java8-installer_8u131-1~webupd8~2_all.deb
+wget -O https://launcher.technicpack.net/launcher4/611/TechnicLauncher.jar
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt upgrade -y
@@ -13,3 +14,6 @@ sudo gdebi -n steam.deb
 sudo gdebi -n java8-installer.deb
 sudo gdebi -n java8-default.deb
 sudo apt install nano
+echo |======================|
+echo |Installation Complete!|
+echo |======================|
