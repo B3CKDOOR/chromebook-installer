ec67236481a0ba5e7c54703d9d84a7bbce6354a6
B3CKDOOR <b3ckdoor@protonmail.com>
2021-02-21 18:11:57 +0100

    forgot to add -O(utput)

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ wget -O steam.deb http://media.steampowered.com/client/installer/steam.deb
 wget -O java8-default.deb https://debian.opennms.org/dists/opennms-25/main/binary-all/oracle-java8-set-default_8u131-1~webupd8~2_all.deb
 wget -O java8-installer.deb https://debian.opennms.org/dists/opennms-25/main/binary-all/oracle-java8-installer_8u131-1~webupd8~2_all.deb
 wget https://launcher.technicpack.net/launcher4/611/TechnicLauncher.jar
-wget visualstudiocode.deb https://az764295.vo.msecnd.net/stable/622cb03f7e070a9670c94bae1a45d78d7181fbd4/code_1.53.2-1613044664_amd64.deb
+wget -O visualstudiocode.deb https://az764295.vo.msecnd.net/stable/622cb03f7e070a9670c94bae1a45d78d7181fbd4/code_1.53.2-1613044664_amd64.deb
 sudo dpkg --add-architecture i386
 sudo add-apt-repository ppa:videolan/master-daily
 sudo apt update
