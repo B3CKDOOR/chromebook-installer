90c08375bb699ecea572a10afab709e1c4043858
B3CKDOOR <b3ckdoor@protonmail.com>
2021-02-21 23:00:37 +0100

    Revert "Simplifying sudo + neofetch"
    This reverts commit 9b5dd3ba79263ed24e94507662008a2419505e58.

--- install.sh
+++ install.sh
@@ -5,27 +5,21 @@ wget -O java8-default.deb https://debian.opennms.org/dists/opennms-25/main/binar
 wget -O java8-installer.deb https://debian.opennms.org/dists/opennms-25/main/binary-all/oracle-java8-installer_8u131-1~webupd8~2_all.deb
 wget https://launcher.technicpack.net/launcher4/611/TechnicLauncher.jar
 wget -O visualstudiocode.deb https://az764295.vo.msecnd.net/stable/622cb03f7e070a9670c94bae1a45d78d7181fbd4/code_1.53.2-1613044664_amd64.deb
-dpkg --add-architecture i386
-sudo su
-cd ~
-add-apt-repository ppa:videolan/master-daily ppa:dawidd0811/neofetch
-apt update
-apt upgrade -y
-apt autoremove
+sudo dpkg --add-architecture i386
+sudo add-apt-repository ppa:videolan/master-daily ppa:dawidd0811/neofetch
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove
 echo debconf shared/accepted-oracle-license-v1-1 select true | \
-debconf-set-selections
+sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-debconf-set-selections
-apt install -y wget gdebi-core libgl1-mesa-glx:i386 vlc qtwayland5 build-essential nano neofetch
-exit
-cp neofetch.conf ~/.config/neofetch/config.conf
-sudo su
-gdebi -n discord.deb
-gdebi -n steam.deb
-gdebi -n java8-installer.deb
-gdebi -n java8-default.deb
-gdebi -n visualstudiocode.deb
+sudo debconf-set-selections
+sudo apt install -y wget gdebi-core libgl1-mesa-glx:i386 vlc qtwayland5 build-essential nano neofetch
+sudo gdebi -n discord.deb
+sudo gdebi -n steam.deb
+sudo gdebi -n java8-installer.deb
+sudo gdebi -n java8-default.deb
+sudo gdebi -n visualstudiocode.deb
 echo #======================
 echo #Installation Complete!
 echo #======================
-exit
