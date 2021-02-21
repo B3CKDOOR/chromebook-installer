c083e836cf24231e64ad961d97e5ec9e0f6c1b37
B3CKDOOR <b3ckdoor@protonmail.com>
2021-02-21 18:23:12 +0100

    added neofetch

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ wget -O java8-installer.deb https://debian.opennms.org/dists/opennms-25/main/bin
 wget https://launcher.technicpack.net/launcher4/611/TechnicLauncher.jar
 wget -O visualstudiocode.deb https://az764295.vo.msecnd.net/stable/622cb03f7e070a9670c94bae1a45d78d7181fbd4/code_1.53.2-1613044664_amd64.deb
 sudo dpkg --add-architecture i386
-sudo add-apt-repository ppa:videolan/master-daily
+sudo add-apt-repository ppa:videolan/master-daily ppa:dawidd0811/neofetch
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove
@@ -14,7 +14,7 @@ echo debconf shared/accepted-oracle-license-v1-1 select true | \
 sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | \
 sudo debconf-set-selections
-sudo apt install -y wget gdebi-core libgl1-mesa-glx:i386 vlc qtwayland5 build-essential nano
+sudo apt install -y wget gdebi-core libgl1-mesa-glx:i386 vlc qtwayland5 build-essential nano neofetch
 sudo gdebi -n discord.deb
 sudo gdebi -n steam.deb
 sudo gdebi -n java8-installer.deb
